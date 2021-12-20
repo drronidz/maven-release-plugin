254efa42699b15b6880ee58efb6c5952786a7056
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-20 22:11:01 +0100

    Initial Commit (2) ...

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# maven-release-plugin
-Maven Release Plugin Configuration
+# Apache Maven: Beginner to Guru
+
+This respository contains code examples for the online course [Apache Maven: Beginner to Guru.](https://www.udemy.com/draft/2043700/?couponCode=GITHUB_REPO)
+
+## Maven Release Plugin Examples
--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -1,11 +1,4 @@
-package cleverdeveloper.springframework;
-
-/*
-PROJECT NAME : maven-release-plugin
-Module NAME: IntelliJ IDEA
-Author Name : @ DRRONIDZ
-DATE : 12/20/2021 9:35 PM
-*/
+package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +6,6 @@ class JavaHelloWorldTest {
 
     @Test
     void getHello() {
-
         System.out.println(new JavaHelloWorld().getHello());
 
     }
