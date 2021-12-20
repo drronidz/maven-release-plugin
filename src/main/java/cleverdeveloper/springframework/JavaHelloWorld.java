e458638da2b8863daf23b9ce911d4e53ce53021a
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-20 21:38:02 +0100

    Initial Commit ...

---
+++ src/main/java/cleverdeveloper/springframework/JavaHelloWorld.java
@@ -0,0 +1,15 @@
+package cleverdeveloper.springframework;
+
+/*
+PROJECT NAME : maven-release-plugin
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 12/20/2021 9:34 PM
+*/
+
+public class JavaHelloWorld {
+
+    public String getHello() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/cleverdeveloper/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,20 @@
+package cleverdeveloper.springframework;
+
+/*
+PROJECT NAME : maven-release-plugin
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 12/20/2021 9:35 PM
+*/
+
+import org.junit.jupiter.api.Test;
+
+class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+
+        System.out.println(new JavaHelloWorld().getHello());
+
+    }
+}
